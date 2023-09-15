c87125f9e8ee34e1b7fd3d7d0c0c349cb752b3a1
makefool <makefool@163.com>
2023-09-15 16:09:22 +0800

    new file:   serial_verify.py

---
+++ serial_verify.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import threading
+
+class pipe_Str:
+    def __init__(self):
+        self.buf = b""
+        self.lock = threading.Semaphore(1)
+    def put(self, inPut):
+        self.lock.acquire()
+        self.buf += inPut
+        self.lock.release
+    def get(self, l):
+        self.lock.acquire()
+        self.buf += inPut
+        self.lock.release
+
+if __name__ == "__main__":
+    pass
