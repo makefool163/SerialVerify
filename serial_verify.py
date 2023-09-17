b1576da4600349c2f91f25fa14d8baeb3ee6e23d
makefool <makefool@163.com>
2023-09-17 19:47:42 +0800

    modified:   idea.txt 	modified:   serial_verify.py

--- serial_verify.py
+++ serial_verify.py
@@ -2,8 +2,8 @@
 from __future__ import absolute_import, print_function
 
 import threading
+import serial
 import eventlet
-
 
 class pipe_Str:
     def __init__(self):
@@ -21,6 +21,24 @@ class pipe_Str:
         return ret
     def qsize(self):
         return len(self.buf)        
+
+class serial_verify:
+    def __init__(self, com_port, baud_rate):
+        self.write_enable = False
+        self.com = serial.Serial(port=com_port, 
+                                timeout = 0, 
+                                # non-blocking mode, return immediately in any case, 
+                                # returning zero or more, up to the requested number of bytes
+                                baudrate=baud_rate,
+                                rtscts=True,
+                                dsrdtr=True,
+                                # We use hardware stream control here
+                                parity=serial.PARITY_EVEN,
+                                bytesize=serial.EIGHTBITS)
+        
+    def write(self, buf):
+
+    def read(self, block=False):
 
 if __name__ == "__main__":
     pass
